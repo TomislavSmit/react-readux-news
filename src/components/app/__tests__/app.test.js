1ffc196d601956442111e16a61e6bc2492d3d2e0
tomislavsmit <schmidt.tomislav@gmail.com>
2020-12-21 11:12:01 +0100

    Add enzyme and test App component

---
+++ src/components/app/__tests__/app.test.js
@@ -0,0 +1,15 @@
+import { shallow } from 'enzyme';
+
+import App from '../app';
+import Header from '../../header/header';
+import AppRouter from '../../../routes';
+
+describe('App', () => {
+  const wrapper = shallow(<App />);
+
+  it('renders Header component', () => {
+    expect(wrapper.find(Header).exists()).toBe(true);
+
+    expect(wrapper.find(AppRouter).exists()).toBe(true);
+  });
+});
